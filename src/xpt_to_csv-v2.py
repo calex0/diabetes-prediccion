3bf7dd4714fc7f026cc2ce1d9aa5a0672ac9a494
calex0 <carlosalej.perez@gmail.com>
2026-06-15 11:48:01 +0200

    Add files via upload

---
+++ src/xpt_to_csv-v2.py
@@ -0,0 +1,10 @@
+import pyreadstat
+
+# Leer archivo XPT con codificación robusta
+df, meta = pyreadstat.read_xport("LLCP2014XPT.XPT", encoding="latin1")
+
+# Guardar como CSV
+df.to_csv("LLCP2014.csv", index=False)
+
+print("Conversión completada.")
+
